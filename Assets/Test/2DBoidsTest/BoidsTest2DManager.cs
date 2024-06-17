629318f05b94d405f06a8c0c38d8e5db8aec1404
david0305 <david0305mc@gmail.com>
2024-06-17 21:27:16 +0900

    Boids2D Test

---
+++ Assets/Test/2DBoidsTest/BoidsTest2DManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TEST
+{
+    public class BoidsTest2DManager : SingletonMono<BoidsTest2DManager>
+    {
+        [SerializeField] private Boids2DObj boidsObj;
+
+        public List<Boids2DObj> boidsObjList = new List<Boids2DObj>();
+
+        private void Start()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Boids2DObj obj = Lean.Pool.LeanPool.Spawn(boidsObj, transform, false);
+                obj.transform.position = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
+                boidsObjList.Add(obj);
+            }
+            
+        }
+
+    }
+
+}
