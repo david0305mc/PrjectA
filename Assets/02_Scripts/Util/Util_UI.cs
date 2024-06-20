ac298600b5f22ec03ead3f8f0d52c7389da5e2ca
david0305 <david0305mc@gmail.com>
2024-06-20 11:59:16 +0900

    UI Util

---
+++ Assets/02_Scripts/Util/Util_UI.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class Utill
+{
+    //RectTransform RectTransform => transform as RectTransform;
+
+    //private async void OrderSibling(int siblingIndex)
+    //{
+    //    RectTransform.SetSiblingIndex(siblingIndex);
+    //    await UniTask.Yield(cancellationToken: this.GetCancellationTokenOnDestroy());
+    //    LayoutRebuilder.MarkLayoutForRebuild(RectTransform);
+    //    Canvas.ForceUpdateCanvases();
+    //}
+
+    //RectTransform RectTransform => transform as RectTransform;
+
+    //private async void SetLastSibling()
+    //{
+    //    RectTransform.SetAsLastSibling();
+    //    await UniTask.WaitForEndOfFrame(cancellationToken: this.GetCancellationTokenOnDestroy());
+    //    LayoutRebuilder.MarkLayoutForRebuild(RectTransform);
+    //    Canvas.ForceUpdateCanvases();
+    //}
+    //public enum BuilderType
+    //{
+    //    MarkLayoutForRebuild,
+    //    ForceRebuildLayoutImmediate
+    //}
+
+    //protected override void OnRectTransformDimensionsChange()
+    //{
+    //    base.OnRectTransformDimensionsChange();
+    //    ForceRebuilder();
+    //}
+
+    //protected async void ForceRebuilder(BuilderType type = BuilderType.MarkLayoutForRebuild)
+    //{
+    //    if (type == BuilderType.MarkLayoutForRebuild)
+    //    {
+    //        await UniTask.Yield(cancellationToken: this.GetCancellationTokenOnDestroy());
+    //        LayoutRebuilder.MarkLayoutForRebuild(RectTransform);
+    //    }
+    //    else
+    //        LayoutRebuilder.ForceRebuildLayoutImmediate(RectTransform);
+
+    //    ForceUpdateCanvas();
+    //}
+
+    //protected void ForceUpdateCanvas()
+    //{
+    //    if (!useForceCanvases) return;
+    //    Canvas.ForceUpdateCanvases();
+    //}
+}
