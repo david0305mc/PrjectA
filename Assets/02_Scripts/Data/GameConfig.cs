e2305096b992d1143aa5c43022e28cd051d6f7d2
david0305 <david0305mc@gmail.com>
2024-06-25 18:25:33 +0900

    불필요한 파일 제거

--- Assets/02_Scripts/Data/GameConfig.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameConfig 
-{
-    public static readonly string LocalTableFolderPath = "Assets/StreamingAssets/";
-
-}
