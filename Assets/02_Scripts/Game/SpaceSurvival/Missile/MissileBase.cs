165d52b31f08dc3ef30f7713f0d99afc4093f4cf
david0305 <david0305mc@gmail.com>
2024-08-27 19:01:48 +0900

    미사일 기본 임시

---
+++ Assets/02_Scripts/Game/SpaceSurvival/Missile/MissileBase.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileBase : MonoBehaviour
+{
+    private TrailRenderer trailRenderer;
+    protected Rigidbody2D rigidBody2d;
+
+    protected float speed;
+    protected AttackData attackData;
+    protected BaseObj targetObj;
+
+    protected Vector2 srcPos;
+    protected Vector2 dstPos;
+    protected Vector2 lastMoveVector;
+    protected Vector2 prevPos;
+
+    protected float dist;
+    private float moveDist;
+    protected float elapse;
+    private bool isDisposed;
+
+    protected virtual void Awake()
+    {
+        rigidBody2d = GetComponent<Rigidbody2D>();
+        trailRenderer = GetComponent<TrailRenderer>();
+    }
+
+    public virtual void Shoot(AttackData _attackData, BaseObj _targetObj, float _speed)
+    {
+        attackData = _attackData;
+        targetObj = _targetObj;
+
+        var unitGradeInfo = DataManager.Instance.GetUnitGrade(attackData.attackerTID, attackData.grade);
+        var projectileInfo = DataManager.Instance.GetProjectileInfoData(unitGradeInfo.projectileid);
+        
+        dstPos = targetObj.transform.position;
+        srcPos = transform.position;
+        dist = Vector2.Distance(srcPos, dstPos);
+        moveDist = Vector2.Distance(srcPos, dstPos);
+        elapse = 0f;
+        speed = _speed;
+        isDisposed = false;
+        if (trailRenderer != null)
+        {
+            trailRenderer.Clear();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateMissile();
+    }
+
+    protected virtual bool UpdateMissile()
+    {
+        if (targetObj != null)
+        {
+            dstPos = targetObj.transform.position;
+        }
+
+        if (elapse >= 1)
+        {
+            float addElapse = (Time.fixedDeltaTime / moveDist) * speed;
+            var moveDelta = lastMoveVector.normalized * addElapse * moveDist;
+
+            //rigidBody2d.transform.position = new Vector2(transform.position.x, transform.position.y) + moveDelta;
+            rigidBody2d.MovePosition(new Vector2(transform.position.x, transform.position.y) + moveDelta);
+            prevPos = transform.position;
+            return false;
+        }
+        return true;
+    }
+
+    protected void Dispose()
+    {
+        if (!isDisposed)
+        {
+            Lean.Pool.LeanPool.Despawn(gameObject);
+            elapse = 0f;
+            isDisposed = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Damageable damagable = collision.GetComponent<Damageable>();
+        if (damagable != null)
+        {
+            if (damagable.IsEnemy())
+            {
+                if (!targetObj.IsHero)
+                {
+                    Hit(collision, damagable);
+                }
+            }
+            else
+            {
+                if (targetObj.IsHero)
+                {
+                    Hit(collision, damagable);
+                }
+            }
+        }
+    }
+
+    private void Hit(Collider2D collision, Damageable damagable)
+    {
+        MGameManager.Instance.ShowBoomEffect(attackData, collision.ClosestPoint(transform.position));
+        MGameManager.Instance.DoAreaAttack(attackData, collision.ClosestPoint(transform.position));
+        damagable.GetDamaged(attackData);
+
+        //targetAttackCount--;
+        //if (targetAttackCount <= 0)
+        //{
+        //    Dispose();
+        //}
+    }
+}
